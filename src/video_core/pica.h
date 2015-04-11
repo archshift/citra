95446449c36c50ec212d2bdfa49da70bddc75c16
archshift <gh@archshift.com>
2015-04-11 16:15:39 -0700

    Implement I4 texture format
    Untested!

--- src/video_core/pica.h
+++ src/video_core/pica.h
@@ -153,7 +153,7 @@ struct Regs {
         I8           =  7,
         A8           =  8,
         IA4          =  9,
-
+        I4           = 10,
         A4           = 11,
         ETC1         = 12,  // compressed
         ETC1A4       = 13,  // compressed
